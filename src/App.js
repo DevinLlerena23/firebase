58dda3fd33bec5698f440027091c68908bd0d5b9
Devinllerena23 <devinllerena12@gmail.com>
2023-02-09 11:15:27 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { db } from './Firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const tareasColeccionRef = collection(db, "tareas");
+
+  useEffect(() => {
+    async function getTareas() {
+      const data = await getDocs(tareasColeccionRef);
+      console.log(data)
+    }
+    getTareas();
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
